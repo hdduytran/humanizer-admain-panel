bef801225ac5fac174851c8082b7e927762dcd1c
Duy Tran <ducduyleo@gmai.com>
2024-02-20 14:17:49 +0700

    Update user functions to use username instead of user_id

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,11 +33,11 @@ def get_users():
     return col_users.find({"active":True}, sort=[("updated_time", pymongo.ASCENDING)])
 
 
-def get_user(user_id):
-    return col_users.find_one({'user_id': user_id})
+def get_user(username):
+    return col_users.find_one({'username': username})
 
-def update_user(user_id, user):
-    col_users.update_one({'user_id': user_id}, {'$set': user}, upsert=True)
+def update_user(username, user):
+    col_users.update_one({'username': username}, {'$set': user}, upsert=True)
 
 # streamlit app
 st.title('Humanizer')
@@ -59,29 +59,29 @@ authenticator.login()
 if st.session_state["authentication_status"]:
     authenticator.logout()
     st.write('# Create a new user')
-    user_ids = st.text_input('Enter user_id')
+    usernames = st.text_input('Enter username')
     if st.button('Create user'):
-        user_ids = user_ids.split(' ')
-        for user_id in user_ids:
-            user_id = user_id.strip()
-            if get_user(user_id):
+        usernames = usernames.split(' ')
+        for username in usernames:
+            username = username.strip()
+            if get_user(username):
                 user = {
-                    "user_id": user_id,   
+                    "username": username,   
                     "active": True,
                     'updated_time': datetime.now()
                 }
-                update_user(user_id, user)
-                st.write(f'User {user_id} already exists, updated user')
+                update_user(username, user)
+                st.write(f'User {username} already exists, updated user')
             else:
                 user = {
-                    'user_id': user_id,
+                    'username': username,
                     "active": True,
                     'created_time': datetime.now(),
                     'updated_time': datetime.now()
                 }
 
                 create_user(user)
-                st.write(f'User {user_id} created successfully')
+                st.write(f'User {username} created successfully')
             
     st.write('# All users')
     users = list(get_users())
@@ -92,20 +92,19 @@ if st.session_state["authentication_status"]:
         df = df.drop(columns=['_id'])
 
         # remove users
-        if "user_id" in df.columns:
-            remove_user_list = st.multiselect('Select users to remove', list(df['user_id']))
-            if st.button('Remove users'):
-                for user in remove_user_list:
-                    update_user(user, {'active': False, 'updated_time': datetime.now()})
-                    st.write(f'{user} removed successfully')
-                    
-                users = list(get_users())
-                df = pd.DataFrame(users)
-                if "_id" in df.columns:
-                    df = df.drop(columns=['_id'])
+        remove_user_list = st.multiselect('Select users to remove', list(df['username']))
+        if st.button('Remove users'):
+            for user in remove_user_list:
+                update_user(user, {'active': False, 'updated_time': datetime.now()})
+                st.write(f'{user} removed successfully')
+                
+            users = list(get_users())
+            df = pd.DataFrame(users)
+            if "_id" in df.columns:
+                df = df.drop(columns=['_id'])
         st.dataframe(df)
         
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your Username and password')
+    st.warning('Please enter your username and password')
